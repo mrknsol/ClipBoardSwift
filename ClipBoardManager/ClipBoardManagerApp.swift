3f2669aaf584b1341b3574be752ee5d44acbc732
mrknsol <kenanmemmedli055@gmail.com>
2024-07-07 22:17:20 +0400

    Initial Commit

---
+++ ClipBoardManager/ClipBoardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBoardManagerApp.swift
+//  ClipBoardManager
+//
+//  Created by mrknsol on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipBoardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
